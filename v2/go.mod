00002e0041dd47767f0d1d42b2a8b181d8e7b0e1
Dmitry Verkhoturov <paskal.07@gmail.com>
2024-02-20 17:30:36 +0100

    migrate v2 to using v3 code instead of duplicating it

--- v2/go.mod
+++ v2/go.mod
@@ -2,10 +2,15 @@ module github.com/go-pkgz/expirable-cache/v2
 
 go 1.20
 
-require github.com/stretchr/testify v1.8.4
+require (
+	github.com/go-pkgz/expirable-cache/v3 v3.0.0
+	github.com/stretchr/testify v1.8.4
+)
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
+
+replace github.com/go-pkgz/expirable-cache/v3 => ../v3
--- v2/options.go
+++ v2/options.go
@@ -12,25 +12,25 @@ type options[K comparable, V any] interface {
 // WithTTL functional option defines TTL for all cache entries.
 // By default, it is set to 10 years, sane option for expirable cache might be 5 minutes.
 func (c *cacheImpl[K, V]) WithTTL(ttl time.Duration) Cache[K, V] {
-	c.ttl = ttl
+	c.Cache.WithTTL(ttl)
 	return c
 }
 
 // WithMaxKeys functional option defines how many keys to keep.
 // By default, it is 0, which means unlimited.
 func (c *cacheImpl[K, V]) WithMaxKeys(maxKeys int) Cache[K, V] {
-	c.maxKeys = maxKeys
+	c.Cache.WithMaxKeys(maxKeys)
 	return c
 }
 
 // WithLRU sets cache to LRU (Least Recently Used) eviction mode.
 func (c *cacheImpl[K, V]) WithLRU() Cache[K, V] {
-	c.isLRU = true
+	c.Cache.WithLRU()
 	return c
 }
 
 // WithOnEvicted defined function which would be called automatically for automatically and manually deleted entries
 func (c *cacheImpl[K, V]) WithOnEvicted(fn func(key K, value V)) Cache[K, V] {
-	c.onEvicted = fn
+	c.Cache.WithOnEvicted(fn)
 	return c
 }
